631f58656ed0bc26d8b6e9c93d42ea4ccedd54fb
RAGINI60 <83510001+RAGINI60@users.noreply.github.com>
2024-04-21 00:13:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app=express();
 config({path:"./config/config.env"});
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:[process.env.https://job-ify.netlify.app/],
     methods:["GET","POST","DELETE","PUT"],
     credentials:true,
 })
